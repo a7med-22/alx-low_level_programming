2d88e8a157705fefa2a490ac6b8b181e7170597a
Ahmed osama <ao6155518@gmail.com>
2023-09-08 16:56:00 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: AC pogram that prints with put function 
+ * Return: Always 0 (succes)
+ */ 
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
